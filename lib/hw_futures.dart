0f052d7825e0cf9d92460e9e1f11f6ebafc6077b
Andrii Vronskyi <vronskyi.a@gmail.com>
2025-07-13 14:58:51 +0300

    feat: implement Task 2 - async age fetching

--- lib/hw_futures.dart
+++ lib/hw_futures.dart
@@ -1,7 +1,9 @@
 void main() async {
   String userName = await fetchName();
-
   print('Мене звати $userName');
+
+  String userAge = await fetchAge();
+  print('Мені $userAge');
 }
 
 // Task 1: Асинхронне отримання імені
@@ -13,7 +15,23 @@ Future<String> fetchName() async {
 }
 
 // Task 2: Асинхронне отримання віку  
-// TODO: Реалізувати fetchAge()
+Future<String> fetchAge() async {
+  await Future.delayed(Duration(milliseconds: 1500));
+  int age = 25;
+  String word = getAgeWord(age);
+  return '$age $word';
+}
+
+String getAgeWord(int age) {
+  if (age % 10 == 1 && age % 100 != 11) {
+    return 'рік';
+  } else if ((age % 10 == 2 || age % 10 == 3 || age % 10 == 4) && 
+             (age % 100 != 12 && age % 100 != 13 && age % 100 != 14)) {
+    return 'роки';
+  } else {
+    return 'років';
+  }
+}
 
 // Task 3: Послідовне виконання Future
 // TODO: Реалізувати sequential execution
@@ -22,4 +40,4 @@ Future<String> fetchName() async {
 // TODO: Реалізувати parallel execution
 
 // Task 5: Зворотний відлік з затримкою
-// TODO: Реалізувати delayedCountdown() 
+// TODO: Реалізувати delayedCountdown()
